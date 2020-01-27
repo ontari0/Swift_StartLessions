5b47361f7fba1febda380c2a54ce6977feaed2f3
Roman <russinov7@gmail.com>
2020-01-27 20:23:13 +0500

    Одно задание

--- L7_RomanRusinov.playground/Contents.swift
+++ L7_RomanRusinov.playground/Contents.swift
@@ -1,7 +1,5 @@
 import UIKit
-
-//Еще в процессе
-
+// в процессе
 enum buyError: Error {
     case noMoney
     case anotherPaymentMethod
@@ -13,23 +11,52 @@ struct jacket {
     var price: Double
 }
 
-class buy{
+class buy {
+    
+    var money = 0.0
+    var paymentMethod = ""
     
     var jackets = [
-        "jacket1": jacket(name: "Black jacket", price: 1999.00),
-        "jacket2": jacket(name: "Black winter jacket", price: 5999.00),
-        "jacket3": jacket(name: "Red jacket", price: 2500.00)
+        "Black jacket": jacket(name: "Black jacket", price: 1999.00),
+        "Black winter jacket": jacket(name: "Black winter jacket", price: 5999.00),
+        "Red jacket": jacket(name: "Red jacket", price: 2500.00)
     ]
     
-    func onlinebuyng(name: String, price: Double) throws{
+    func onlinebuyng(name: String, payment: String) throws{
         guard let jacket = jackets[name] else {
             throw buyError.notFound
         }
-        guard jacket.price  else {
+        guard paymentMethod == payment else {
+            throw buyError.anotherPaymentMethod
+        }
+        guard jacket.price <= money else {
             throw buyError.noMoney
         }
+        
+        print("Your order \(jacket.name) has been paid")
     }
 }
 
-var item1: buy
+func buying(item: String, payment: String, buying: buy) throws {
+    try buying.onlinebuyng(name: item, payment: payment)
+}
+
+var customer = buy()
+
+customer.money = 2000.00
+
+do {
+    try buying(item: "Black winter jacket", payment: "Cash", buying: customer)
+}
+
+catch buyError.notFound {
+    print("This item was not found")
+}
+catch buyError.anotherPaymentMethod {
+    print("This payment method is not supported")
+}
+catch buyError.noMoney {
+    print("Not enough money")
+}
+
 
